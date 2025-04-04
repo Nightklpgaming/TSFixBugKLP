19e8acc13cd72963fbcb0db096164959d54c6989
Nightklp <120942242+Nightklpgaming@users.noreply.github.com>
2025-04-04 22:47:17 +0800

    Add files via upload

---
+++ TSFixBugKLP/Config.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TShockAPI;
+
+namespace TSFixBugKLP
+{
+
+    public class Config
+    {
+        public CONFIG_MAIN Main;
+
+        static string path = Path.Combine(TShock.SavePath, "TSFixBugKLP.json");
+
+        public static Config Read()
+        {
+            if (!File.Exists(path))
+            {
+                File.WriteAllText(path, JsonConvert.SerializeObject(Default(), Formatting.Indented));
+                return Default();
+            }
+
+            var args = JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
+
+            if (args == null) return Default();
+
+            if (args.Main == null) args.Main = new();
+            args.Main.FixNull();
+
+            File.WriteAllText(path, JsonConvert.SerializeObject(args, Formatting.Indented));
+            return args;
+        }
+
+        /// <summary>
+        /// changes config file
+        /// </summary>
+        /// <param name="config"></param>
+        public void Changeall(Config config)
+        {
+            if (!File.Exists(path))
+            {
+                File.WriteAllText(path, JsonConvert.SerializeObject(Default(), Formatting.Indented));
+            }
+            else
+            {
+                File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented));
+            }
+        }
+
+        private static Config Default()
+        {
+            return new Config()
+            {
+                Main = new CONFIG_MAIN(),
+            };
+        }
+    }
+
+    #region [ Config Objects ]
+
+    public class CONFIG_MAIN
+    {
+        public bool? Enable_ItemFrameFix = true;
+        public bool? Enable_WeaponRackFix = true;
+        public bool? Enable_FixCPlayerG = true;
+        public CONFIG_MAIN() { }
+
+        public void FixNull()
+        {
+            CONFIG_MAIN getdefault = new();
+
+            if (Enable_ItemFrameFix == null) Enable_ItemFrameFix = getdefault.Enable_ItemFrameFix;
+            if (Enable_WeaponRackFix == null) Enable_WeaponRackFix = getdefault.Enable_WeaponRackFix;
+            if (Enable_FixCPlayerG == null) Enable_FixCPlayerG = getdefault.Enable_FixCPlayerG;
+        }
+    }
+
+    #endregion
+}
---
+++ TSFixBugKLP/TSFixBugKLP.cs
@@ -0,0 +1,162 @@
+﻿using Mysqlx.Crud;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI.Hooks;
+using TShockAPI;
+using Terraria.Localization;
+using Terraria.GameContent.Tile_Entities;
+using Terraria.DataStructures;
+
+namespace TSFixBugKLP
+{
+    [ApiVersion(2, 1)]
+    public class TSFixBugKLP : TerrariaPlugin
+    {
+        #region =[ Plugin Info ]=
+        public override string Author => "NightKLP";
+
+        public override string Description => "fix a certain tshock simple bug";
+
+        public override string Name => "TSFixBugKLP";
+
+        public override Version Version => new System.Version(1, 0);
+
+        #endregion
+
+        public static Config Config = Config.Read(); //CONFIG
+
+        public TSFixBugKLP(Main game) : base(game)
+        {
+            //amogus
+        }
+
+        #region [ Initialize ]
+        public override void Initialize()
+        {
+            ServerApi.Hooks.NetGetData.Register(this, OnGetData);
+
+            GeneralHooks.ReloadEvent += OnReload;
+        }
+        #endregion
+
+        #region [ Dispose ]
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.NetGetData.Deregister(this, OnGetData);
+
+                GeneralHooks.ReloadEvent -= OnReload;
+            }
+            base.Dispose(disposing);
+        }
+        #endregion
+
+        #region [ Reload ]
+
+        private void OnReload(ReloadEventArgs args)
+        {
+            #region code
+            Config = Config.Read();
+            args.Player.SendInfoMessage("TSFixBugKLP Config reloaded!");
+            #endregion
+        }
+
+        #endregion
+
+        private async void OnGetData(GetDataEventArgs args)
+        {
+            if (args.Handled)
+                return;
+
+            #region { Place Item Frame }
+            if (args.MsgID == PacketTypes.PlaceItemFrame && (bool)Config.Main.Enable_ItemFrameFix)
+            {
+                TSPlayer player = TShock.Players[args.Msg.whoAmI];
+                using (var stream = new MemoryStream(args.Msg.readBuffer, args.Index, args.Length))
+                {
+                    using (var reader = new BinaryReader(stream))
+                    {
+                        short X = reader.ReadInt16();
+                        short Y = reader.ReadInt16();
+                        short ItemID = reader.ReadInt16();
+                        byte Prefix = reader.ReadByte();
+                        short Stack = reader.ReadInt16();
+
+                        if (!player.HasBuildPermission(X, Y))
+                        {
+                            int num = Item.NewItem(null, (X * 16) + 8, (Y * 16) + 8, player.TPlayer.width, player.TPlayer.height, ItemID, Stack, noBroadcast: true, Prefix, noGrabDelay: true);
+                            Main.item[num].playerIndexTheItemIsReservedFor = player.Index;
+                            NetMessage.SendData((int)PacketTypes.ItemDrop, player.Index, -1, NetworkText.Empty, num, 1f);
+                            NetMessage.SendData((int)PacketTypes.ItemOwner, player.Index, -1, NetworkText.Empty, num);
+                        }
+                    }
+                }
+                return;
+            }
+            #endregion
+
+            #region { Weapons Rack Try Placing }
+            if (args.MsgID == PacketTypes.WeaponsRackTryPlacing && (bool)Config.Main.Enable_WeaponRackFix)
+            {
+                TSPlayer player = TShock.Players[args.Msg.whoAmI];
+                using (var stream = new MemoryStream(args.Msg.readBuffer, args.Index, args.Length))
+                {
+                    using (var reader = new BinaryReader(stream))
+                    {
+                        short X = reader.ReadInt16();
+                        short Y = reader.ReadInt16();
+                        short ItemID = reader.ReadInt16();
+                        byte Prefix = reader.ReadByte();
+                        short Stack = reader.ReadInt16();
+                        TEWeaponsRack WeaponRack = (TEWeaponsRack)TileEntity.ByID[TEWeaponsRack.Find(X, Y)];
+
+                        if (!player.HasBuildPermission(X, Y))
+                        {
+                            int num = Item.NewItem(null, (X * 16) + 8, (Y * 16) + 8, player.TPlayer.width, player.TPlayer.height, ItemID, Stack, noBroadcast: true, Prefix, noGrabDelay: true);
+                            Main.item[num].playerIndexTheItemIsReservedFor = player.Index;
+                            NetMessage.SendData((int)PacketTypes.ItemDrop, player.Index, -1, NetworkText.Empty, num, 1f);
+                            NetMessage.SendData((int)PacketTypes.ItemOwner, player.Index, -1, NetworkText.Empty, num);
+
+                            NetMessage.SendData((int)PacketTypes.UpdateTileEntity, -1, -1, NetworkText.Empty, WeaponRack.ID, 0, 1);
+                            args.Handled = true;
+                        }
+                    }
+                }
+                return;
+            }
+            #endregion
+
+            #region { Player Spawn }
+            if (args.MsgID == PacketTypes.PlayerSpawn && (bool)Config.Main.Enable_FixCPlayerG)
+            {
+                using (var stream = new MemoryStream(args.Msg.readBuffer, args.Index, args.Length))
+                {
+                    using (var reader = new BinaryReader(stream))
+                    {
+                        byte playerid = reader.ReadByte();
+                        short spawnX = reader.ReadInt16();
+                        short spawnY = reader.ReadInt16();
+                        int respawnTimer = reader.ReadInt32();
+                        short numberOfDeathsPVE = reader.ReadInt16();
+                        short numberOfDeathsPVP = reader.ReadInt16();
+                        PlayerSpawnContext context = (PlayerSpawnContext)reader.ReadByte();
+
+                        if (context == PlayerSpawnContext.ReviveFromDeath)
+                        {
+                            await fixp();
+                            async Task fixp()
+                            {
+                                await Task.Delay(2000);
+                                NetMessage.SendData((int)PacketTypes.PlayerUpdate, -1, -1, NetworkText.Empty, playerid);
+                            }
+                        }
+
+                    }
+                }
+                return;
+            }
+            #endregion
+        }
+    }
+}
